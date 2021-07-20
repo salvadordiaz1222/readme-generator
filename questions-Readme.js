c0bb052e9ebbf9cb0cfe7e5aded854f481ff1a6c
salvadordiaz1222 <salvador.diaz1222@gmail.com>
2021-07-20 14:30:25 -0500

    The questions the user will need to answer for the README file

---
+++ questions-Readme.js
@@ -0,0 +1,13 @@
+module.exports = {
+    textInput: [
+        {
+            type: "input",
+            name: "title",
+            message: "Please enter your project title?"
+        },
+        {
+            type: "input",
+            name: 
+        }
+    ]
+}
