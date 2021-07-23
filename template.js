634f5dd4a7742c46821e5ee80a1e26b25b83f7a0
salvadordiaz1222 <salvador.diaz1222@gmail.com>
2021-07-22 23:28:19 -0500

    The template of the readme file is ready

--- template.js
+++ template.js
@@ -1,37 +1,56 @@
 module.exports = {
     generateMD: (answers) => {
-        return `
-            # ${answers.title}
-            ## Description
-            Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
-            - What was your motivation?
-            - Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
-            - What problem does it solve?
-            - What did you learn?
-            ## Table of Contents (Optional)
-            If your README is long, add a table of contents to make it easy for users to find what they need.
-            - [Installation](#installation)
-            - [Usage](#usage)
-            - [Credits](#credits)
-            - [License](#license)
-            ## Installation
-            What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
-            ## Usage
-            Provide instructions and examples for use. Include screenshots as needed.
-            ## License
-            The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
-            ---
-            🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
-            ## How to Contribute
-            If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
-            ## Tests
-            Go the extra mile and write tests for your application. Then provide examples on how to run them here.
-            ##Questions
-            GitHub link profile
-            Display email address
-            response these questions:
-            When is the best time to reach you?
-            How long does it takes you to respond?
-        `
+        function badge(type) {
+            const licenses = {
+            "MIT": "[![License: MIT ](https://img.shields.io/badge/License-MIT-yellow.svg)]",
+            "IPL": "[![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)]",
+            "Apache": "[![License: Apache](https://img.shields.io/badge/License-Apache%202.0-blue.svg)]",
+            "MPL 2.0": "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)]"
+            }
+            return licenses[type];
+            }
+
+    return `
+# <${answers.title}>
+
+## Description
+    
+${answers.description}
+    
+## Table of Contents
+    
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contribute)
+- [Tests](#tests)
+- [Questions](#questions)
+    
+## Installation
+    
+${answers.installation}
+    
+## Usage
+    
+${answers.usage}
+![${answers.title}](${answers.image})
+    
+## License
+${badge(answers.license)}
+    
+## Contribute
+    
+${answers.contribute}
+    
+## Tests
+    
+${answers.test}
+    
+## Questions
+    
+[${answers.github}](${answers.github})
+${answers.email}
+You can reach me at my GitHub profile or by email. I usually respond within one day.
+    `
     }
 }
